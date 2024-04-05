a3ff25284f2373fdcde0256e714b0e346f5822bd
Jpark <web.jpark@gmail.com>
2024-04-04 18:35:31 -0600

    creeate some UI components

---
+++ components/Loading.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="loading">
+		<span></span>
+	</div>
+</template>
+<script>
+export default {
+	
+}
+</script>
---
+++ components/Search.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="container search">
+		<input
+			@keyup.enter="$fetch"
+			type="text"
+			placeholder="Search"
+			v-model.lazy="searchInput"
+		/>
+		<button v-show="searchInput !== ''" @click="clearSearch" class="button">
+			Clear Search
+		</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Search",
+};
+</script>
